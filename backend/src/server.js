d59d1887eca6b78f8ac02c7cee70228620907972
Emerson Pereira <edap.diego@gmail.com>
2019-11-01 18:58:53 -0300

    incluindo nova dependencia Cors utilizadada para liberar acesso

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
 const express = require ('express');
 const mongoose = require ('mongoose'); // importando a dependencia 
+const cors = require ('cors');
 const routes = require ('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb://<user>:<password>@omnistack-shard-00-00-xiuxl.mongodb.net:27017,omnistack-shard-00-01-xiuxl.mongodb.net:27017,omnistack-shard-00-02-xiuxl.mongodb.net:27017/test?ssl=true&replicaSet=OmniStack-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect('mongodb://omnistack:omnistack@omnistack-shard-00-00-xiuxl.mongodb.net:27017,omnistack-shard-00-01-xiuxl.mongodb.net:27017,omnistack-shard-00-02-xiuxl.mongodb.net:27017/test?ssl=true&replicaSet=OmniStack-shard-0&authSource=admin&retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -15,6 +16,7 @@ mongoose.connect('mongodb://<user>:<password>@omnistack-shard-00-00-xiuxl.mongod
 // req.params = acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação e edição)
 
+app.use(cors());
 app.use(express.json()); // informar que o retorno será um json - utilizada form json
 
 app.use(routes); // deve vir depois do express.json
