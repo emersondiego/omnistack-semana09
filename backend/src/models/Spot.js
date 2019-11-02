1a4712b31cc11d5eb42e407176d2fb6f0d7ae7dc
Emerson Pereira <edap.diego@gmail.com>
2019-11-02 14:04:32 -0300

    alteração backend para criar url virtual de imagens passando uma nova rota

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -11,6 +11,17 @@ const SpotSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId, // id do usuario que criou o spot
     ref: 'User'
   }
+}, {
+  toJSON: {
+    virtuals: true,
+  },
 });
+
+// criar um virtual, dados que o javascrip vai consumir mas que nao vai ser gravado em banco
+SpotSchema.virtual('thumbnail_url').get(function(){
+  return `http://localhost:3333/files/${this.thumbnail}`
+
+})
+
 
 module.exports = mongoose.model('Spot', SpotSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ const express = require ('express');
 const mongoose = require ('mongoose'); // importando a dependencia 
 const cors = require ('cors');
 const routes = require ('./routes');
+const path = require ('path');
+
 
 const app = express();
 
@@ -20,6 +22,7 @@ app.use(cors());
 app.use(express.json()); // informar que o retorno será um json - utilizada form json
 
 app.use(routes); // deve vir depois do express.json
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads'))); // retorna arquivos estaticos 
 app.listen(3333);
 
 
